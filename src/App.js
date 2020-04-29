7dfc4122caccee311b37315d540234a6f8864412
Dmitry Voronov <voronov.dmitry@gmail.com>
2020-04-29 19:09:25 -0400

    alert

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, {Component} from 'react';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
-import Search from './components/users/Search'
+import Search from './components/users/Search';
+import Alert from './components/layout/Alert';
 import axios from 'axios';
 import './App.css';
 
@@ -11,7 +12,8 @@ import './App.css';
 class App extends Component{
   state = {
     users: [],
-    loading: false
+    loading: false,
+    alert: null
   }
 
 
@@ -29,6 +31,7 @@ class App extends Component{
   }
  
   searchUsers = async(text)=>{
+   
     this.setState({loading: true})
     // console.log(text)
     const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id =${
@@ -39,6 +42,17 @@ class App extends Component{
     this.setState({users: res.data.items, loading: false});
   }
 
+  clearUsers=()=> {
+    this.setState({users:[], loading:false})
+    // this.searchUsers()
+  }
+
+
+  setAlert=(msg, type)=>{
+    this.setState({alert:{msg: msg, type: type}});
+    setTimeout(()=>this.setState({alert:null}), 2000);
+  };
+
   render(){   
   return (
     <div className="App">
@@ -46,7 +60,13 @@ class App extends Component{
       <Navbar />
      
       <div className = "container">
-      <Search searchUsers ={this.searchUsers} />
+        <Alert alert = {this.state.alert} />
+      <Search 
+      searchUsers ={this.searchUsers} 
+      clearUsers = {this.clearUsers} 
+      showClear ={this.state.users.length > 0 ? true : false} 
+      setAlert = {this.setAlert}/>
+
       <Users loading ={this.state.loading} users = {this.state.users} />
       </div>
     </div>
--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -7,15 +7,21 @@ export class Search extends Component {
 
     onSubmit = (e)=>{
         e.preventDefault();
+
         // console.log(this.state.text)
+        if(this.state.text ===''){
+            this.props.setAlert('Please enter something', 'Light');
+        }else{
         this.props.searchUsers(this.state.text);
         this.setState({text: ''});
     }
-
+}
     onChange = (e)=>{
         this.setState({text: e.target.value});
     };
     render() {
+        const {showClear, clearUsers} = this.props;
+
         return (
             <div>
                 <form onSubmit = {this.onSubmit} className="form">
@@ -31,6 +37,10 @@ export class Search extends Component {
                     className='btn btn-dark btn-block'
                      />
                 </form>
+                {showClear && (<button 
+                className= 'btn rbtn-light btn-block' 
+                onClick={clearUsers}>Clear Search Results</button>)}
+               
             </div>
         )
     }
